bd17a43c0cc9b0fabfb372eeeaf64620dab51807
Guido Valente <guidovalente@outlook.com>
2019-01-26 11:36:33 -0300

    render basico del template

--- municipalidad/app/preocupacionales/__init__.py
+++ municipalidad/app/preocupacionales/__init__.py
@@ -1,15 +1,18 @@
 import functools
 
 from flask import (
-    Blueprint, render_template, url_for
+    Blueprint, render_template, url_for, redirect
 )
 
 from flask_wtf import FlaskForm
-from .modelos import Agente
 
 bp = Blueprint('preocupacionales', __name__, url_prefix='/preocupacionales')
 
 @bp.route('/nuevo_agente', methods=('GET', 'POST'))
 def nuevo_agente():
-
-    return render_template('preocupacionales/nuevo_agente.html')
+    from .formularios import SimpleForm
+    form = SimpleForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('preocupacionales/nuevo_agente.html', form=form)
+    # return render_template('preocupacionales/nuevo_agente.html')
