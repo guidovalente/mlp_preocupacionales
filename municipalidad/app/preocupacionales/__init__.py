0e5dbe74675deb5e296983cd23167c2c62058956
Guido Valente <guidovalente@outlook.com>
2019-01-27 21:31:47 -0300

    correcciones del formulario, ya puede guardar en DB, pruebas realizadas

--- municipalidad/app/preocupacionales/__init__.py
+++ municipalidad/app/preocupacionales/__init__.py
@@ -1,18 +1,51 @@
 import functools
 
 from flask import (
-    Blueprint, render_template, url_for, redirect
+    Blueprint, render_template, url_for, redirect, flash
 )
-
-from flask_wtf import FlaskForm
 
 bp = Blueprint('preocupacionales', __name__, url_prefix='/preocupacionales')
 
 @bp.route('/nuevo_agente', methods=('GET', 'POST'))
 def nuevo_agente():
-    from .formularios import SimpleForm
-    form = SimpleForm()
+    # Prueba de modelos
+    # from .modelos import Agente, Reparticion
+    # guido = Agente(
+    #     nombre="Guido",
+    #     apellido="Valente",
+    #     dni=37151819,
+    #     reparticion=Reparticion(nombre="dgp")
+    # )
+
+    from .formularios import FormularioAgente
+    form = FormularioAgente(meta={'locales': ['es_ES', 'es']})
     if form.validate_on_submit():
+        # print(form.nombre.text)
+        from .modelos import Agente, Reparticion, db
+        # nuevo_agente = Agente(
+        #     nombre="Guido",
+        #     apellido="Valente",
+        #     dni=234234234,
+        #     reparticion=reparticion
+        # )
+        # nuevo_agente = Agente(
+        #     nombre=form.nombre.data,
+        #     apellido=form.apellido.data,
+        #     dni=form.dni.data,
+        #     reparticion=db.session.query(Reparticion).filter_by(nombre=form.reparticion.data).first()
+        # )
+        nuevo_agente = Agente()
+        form.populate_obj(nuevo_agente)
+        nuevo_agente.reparticion = db.session.query(Reparticion).filter_by(nombre=form.reparticion.data).first()
+
+        db.session.add(nuevo_agente)
+        db.session.commit()
         return redirect('/')
+    else:
+        if form.errors:
+            print(form.errors)
+            print("Validacion")
+            for field, value in form.errors.items():
+                print(field, value)
+                # for
     return render_template('preocupacionales/nuevo_agente.html', form=form)
-    # return render_template('preocupacionales/nuevo_agente.html')
--- municipalidad/app/preocupacionales/formularios.py
+++ municipalidad/app/preocupacionales/formularios.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import (
-    StringField, IntegerField, DateTimeField, BooleanField, SelectField
+    StringField, IntegerField, DateTimeField, BooleanField, SelectField,
+    TextAreaField
 )
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Optional
 
 class SimpleForm(FlaskForm):
     # name = StringField('name', validators=[DataRequired()])
@@ -27,22 +28,26 @@ class FormularioAgente(FlaskForm):
     domicilio_numero = StringField('Número')
     domicilio_piso = StringField('Piso')
     domicilio_depto = StringField('Departamento')
-    legajo = IntegerField('Legajo')
+    legajo = IntegerField('Legajo', validators=[Optional()])
     reparticion = StringField('Repartición', validators=[InputRequired()])
-    turno_psi_1 = DateTimeField('1º Turno', format='%d/%m/%Y %H:%M')
+    turno_psi_1 = DateTimeField('1º Turno', format='%d/%m/%Y %H:%M',
+        validators=[Optional()])
     ausente_psi_1 = BooleanField('Ausente')
-    turno_psi_2 = DateTimeField('2º Turno', format='%d/%m/%Y %H:%M')
+    turno_psi_2 = DateTimeField('2º Turno', format='%d/%m/%Y %H:%M',
+        validators=[Optional()])
     ausente_psi_2 = BooleanField('Ausente')
     apto_psi = SelectField(
         'Apto',
-        choices=[(None, '-'), (True, 'Sí'), (False, 'No')]
+        choices=[('None', '-'), ('True', 'Sí'), ('False', 'No')]
     )
-    turno_med_1 = DateTimeField('1º Turno', format='%d/%m/%Y %H:%M')
+    turno_med_1 = DateTimeField('1º Turno', format='%d/%m/%Y %H:%M',
+        validators=[Optional()])
     ausente_med_1 = BooleanField('Ausente')
-    turno_med_2 = DateTimeField('2º Turno', format='%d/%m/%Y %H:%M')
+    turno_med_2 = DateTimeField('2º Turno', format='%d/%m/%Y %H:%M',
+        validators=[Optional()])
     ausente_med_2 = BooleanField('Ausente')
     apto_med = SelectField(
         'Apto',
-        choices=[(None, '-'), (True, 'Sí'), (False, 'No')]
+        choices=[('None', '-'), ('True', 'Sí'), ('False', 'No')]
     )
-    observaciones = StringField('Observaciones')
+    observaciones = TextAreaField('Observaciones')
