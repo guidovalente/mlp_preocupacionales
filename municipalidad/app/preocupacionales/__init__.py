929fa857fa989860d99f84eca358b43b7ef04fa1
Guido Valente <guidovalente@outlook.com>
2019-02-02 17:48:12 -0300

    se puebla el select de reparticiones

--- municipalidad/app/preocupacionales/__init__.py
+++ municipalidad/app/preocupacionales/__init__.py
@@ -9,7 +9,19 @@ bp = Blueprint('preocupacionales', __name__, url_prefix='/preocupacionales')
 @bp.route('/nuevo_agente', methods=('GET', 'POST'))
 def nuevo_agente():
     from .formularios import FormularioAgente
+    from .modelos import Reparticion
+
+    # Inicializamos el formulario y luego le asignamos las opciones
+    # de repartición. Al principio le insertamos una opción vacía
+    # para que no comience con ninguna repartición elegida.
     form = FormularioAgente()
+    reparticiones = [(0, '-')] + Reparticion.query.with_entities(
+        Reparticion.id,
+        Reparticion.nombre
+    ).all()
+    if len(reparticiones) < 1:
+        flash('No existen reparticiones cargadas, por lo que no podrá guardar nuevos agentes.')
+    form.reparticion.choices = reparticiones
     if form.validate_on_submit():
         nuevo_agente = Agente()
         form.populate_obj(nuevo_agente)
