e98bbda200a48c3b1c03d614d8440a375d338a97
Guido Valente <guidovalente@outlook.com>
2019-02-10 23:42:59 -0300

    agregada funcionalidad de eliminación de agente

--- municipalidad/app/preocupacionales/__init__.py
+++ municipalidad/app/preocupacionales/__init__.py
@@ -1,5 +1,6 @@
 from flask import (
-    Blueprint, render_template, url_for, redirect, flash, current_app, abort
+    Blueprint, render_template, url_for, redirect, flash, current_app, abort,
+    request
 )
 from sqlalchemy.exc import IntegrityError
 
@@ -44,29 +45,40 @@ def editar_agente(id):
 
     from .formularios import FormularioAgente
     form = FormularioAgente(obj=agente)
-    if form.validate_on_submit():
+    if request.method == 'POST':
         from .modelos import db
-        db.session.add(agente)
-        form.populate_obj(agente, agente_nuevo=True)
-        try:
-            db.session.commit()
-            flash('Los cambios fueron guardados',
-                'border text-center text-success mb-3')
-        except IntegrityError as e:
-            db.session.rollback()
-            agente_existente = Agente.query.filter_by(
-                dni=form.dni.data).first()
-            if agente_existente is not None:
-                error = 'Ya existe un agente con el DNI {} ({} {})'.format(
-                    form.dni.data, agente_existente.nombre,
-                    agente_existente.apellido
-                )
-                form.dni.errors.append("DNI original: {}".format(agente.dni))
-            else:
-                error = e
-            flash(error, 'border text-center text-danger mb-3')
-    elif form.errors:
-        flash(form.errors)
+        if request.form['action'] == 'guardar':
+            if form.validate():
+                db.session.add(agente)
+                form.populate_obj(agente, agente_nuevo=True)
+                try:
+                    db.session.commit()
+                    flash('Los cambios fueron guardados',
+                        'border text-center text-success mb-3')
+                except IntegrityError as e:
+                    db.session.rollback()
+                    agente_existente = Agente.query.filter_by(
+                        dni=form.dni.data).first()
+                    if agente_existente is not None:
+                        error = 'Ya existe un agente con el DNI {} ({} {})' \
+                            .format(form.dni.data, agente_existente.nombre,
+                                agente_existente.apellido)
+                        form.dni.errors.append("DNI original: {}".format(
+                            agente.dni
+                        ))
+                    else:
+                        error = e
+                    flash(error, 'border text-center text-danger mb-3')
+        elif request.form['action'] == 'eliminar':
+            db.session.delete(agente)
+            try:
+                db.session.commit()
+                flash('Agente eliminado.',
+                    'border text-center text-success mb-3')
+                return redirect(url_for('preocupacionales.lista'))
+            except:
+                flash('error sin identificar')
+
     return render_template('preocupacionales/editar_agente.html', form=form,
         id=id)
 
