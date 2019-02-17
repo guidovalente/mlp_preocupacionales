06efc22b70222ee0ea16084d17973dcef9cb8788
Guido Valente <guidovalente@outlook.com>
2019-02-17 16:50:24 -0300

    se agrega comando de insercion de registros de prueba

--- municipalidad/app/modelo_base.py
+++ municipalidad/app/modelo_base.py
@@ -42,6 +42,33 @@ def init_db_command():
     db.session.bulk_save_objects(calendarios)
     db.session.commit()
 
+@click.command('insert-test-records')
+@with_appcontext
+def insert_test_records():
+    nombres = ['Pedro', 'María', 'Martina', 'Lucas', 'Victoria', 'Guido',
+        'Matías', 'Lucrecia']
+    apellidos = ['Quattrini', 'Varlotta', 'Morón', 'Valente', 'Smeriglio',
+        'Zappa']
+    import random
+    from .preocupacionales.modelos import Agente, Reparticion
+    reparticiones = Reparticion.query.count()
+    print("Comenzando inserción de registros de prueba")
+    import sys
+    for i in range(300):
+        print("Insertando registro {}".format(str(i+1)), end='\r', flush=True)
+        agente = Agente.con_turnos(
+            nombre=random.choice(nombres),
+            apellido=random.choice(apellidos),
+            dni=random.randint(1,99999999),
+            telefono=random.randint(1,7777777),
+            legajo=random.randint(717000,717999),
+            reparticion_id=random.randint(1, reparticiones),
+            observaciones="Agente de prueba."
+        )
+        db.session.add(agente)
+        db.session.commit()
+
 
 def register_commands(app):
     app.cli.add_command(init_db_command)
+    app.cli.add_command(insert_test_records)
